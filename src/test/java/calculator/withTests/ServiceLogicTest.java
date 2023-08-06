f0feb0f3bd71cfece42acc275caea54d51f6876a
mikha <Mikhailov.dzenphone@gmail.com>
2023-08-06 12:48:13 +0300

    addClassTest

---
+++ src/test/java/calculator/withTests/ServiceLogicTest.java
@@ -0,0 +1,24 @@
+package calculator.withTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceLogicTest {
+
+    @Test
+    void summ() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void devide() {
+    }
+}
